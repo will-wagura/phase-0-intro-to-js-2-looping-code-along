5258535703268b86fea935fff015c859be974dcd
William Ndirangu <wwagura0@gmail.com>
2024-03-15 05:24:01 +0300

    Loops

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Code your solutions in this file
+function writeCards(
+  names = ["Guadalpe", "Ollie", "Aki"],
+  eventName = ["surprise"]
+) {
+  let messages = [];
+  for (let i = 0; i < names.length; i++) {
+    let message = `Thank you, ${names[i]}, for the wonderful ${eventName} gift!`;
+    messages.push(message);
+  }
+  return messages;
+}
+
+function countDown(number) {
+  let i = number;
+  while ((i) >= 0) {
+    console.log(i);
+    i--;
+  }
+}
